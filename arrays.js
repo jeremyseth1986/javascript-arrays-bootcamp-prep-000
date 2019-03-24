5e271047badc1c39d7f171597d5979ef7472b3de
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-24 02:17:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 chocolateBars.push("twix");
 
 function addElementToBeginningOfArray(array, element) {
-   addElementToBeginningOfArray.unshift();
-   return addElementToBeginningOfArray;
+  
 }
-addElementToBeginningOfArray([1, 2, 3] , 4);
+
