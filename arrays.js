20ccc9818e9cc99d8c246a8589f6235156be44b9
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-24 02:20:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 chocolateBars.push("twix");
 
 function addElementToBeginningOfArray(array, element) {
-  
+  [element,...array];
 }
 
